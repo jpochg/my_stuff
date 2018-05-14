5ac545ddac35a8c5147f3397fcc5573d1c008983
jpochg <39263811+jpochg@users.noreply.github.com>
2018-05-14 12:45:14 +0200

    Function for reading fixed-width text file.
    Function with 2 arguments:
    - path/fixed-width text file 
    - path/schema.csv (Fields: Name, Start Position, End Position, Type)

---
+++ fread_fwf.R
@@ -0,0 +1,21 @@
+fread_fwf<-function(file, schema) {
+  sc<-read.table(file=schema, header=TRUE, sep=";") #schema fwf
+  variables<- as.character(sc[,1])  ##Variables
+  start_col<-sc[,2] ##Posicion inicial
+  end_col<-sc[,3] ##Posicion inicial
+  start_end <- cbind(start_col, end_col) 
+  
+  datos<-  fread(file=file,     
+                   colClasses = "character", 
+                   sep = "\n", 
+                   header = FALSE)
+  
+ extrae<-function(x) {
+    apply(start_end, 1, function(y) stri_sub(x, y[1], y[2]))}
+   
+ datos[, (variables) := data.table((lapply(datos, extrae))$V1)] [,V1:=NULL]
+ 
+ rm(extrae)
+ 
+ return(datos)
+}
